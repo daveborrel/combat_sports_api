027c75ee5ace79cac470efcc2785975cc79405f9
daveborrel <dborrel@student.ubc.ca>
2024-07-26 15:17:21 -0700

    updated json object

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 const PORT = 8000;
-const URL = 'https://www.espn.com/mma/schedule/_/league/ufc';
+const URL = 'https://www.espn.com/mma/schedule';
 
 axios(URL)
     .then(response => {
@@ -12,18 +12,23 @@ axios(URL)
         const $ = cheerio.load(html);
         const fights = [];
 
-        // AnchorLink works
+        // AnchorLink workslol
 
         $('.Table__TR.Table__TR--sm.Table__even', html).each(function () {
             const fight_name = $(this).children('.event__col.Table__TD').text();
             const url = $(this).find('a').attr('href');
             const date = $(this).find('span').text();
+            const dateObject = new Date(date + " " + new Date().getFullYear())
 
-            fights.push({
-                title: fight_name,
-                date: date,
-                url: url
-            })
+            let currentDate = new Date();
+
+            if (dateObject > currentDate) {
+                fights.push({
+                    title: fight_name,
+                    date: dateObject.toDateString(),
+                    url: url
+                })
+            }
         });
 
         console.log(fights);
